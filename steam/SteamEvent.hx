1585b836412c6ae4b355cd2d8b6a5caaa0ecd12d
YellowAfterlife <yellowafterlife@ymail.com>
2016-08-17 07:14:33 +0300

    Added LOBBY_JOIN_REQUESTED event

--- steam/SteamEvent.hx
+++ steam/SteamEvent.hx
@@ -8,6 +8,7 @@ package steam;
 extern class SteamEvent {
 	static inline var USER_PERSONA_NAME:String = "user_persona_name";
 	static inline var LOBBY_JOINED:String = "lobby_joined";
+	static inline var LOBBY_JOIN_REQUESTED:String = "lobby_join_requested";
 	static inline var LOBBY_CREATED:String = "lobby_created";
 	static inline var LOBBY_LIST:String = "lobby_list";
 	static inline var P2P_SESSION_REQUEST:String = "p2p_session_request";
