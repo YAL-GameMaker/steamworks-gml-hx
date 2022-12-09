b1830562f6befe93b744dc3755bb55d70575b8ae
YellowAfterlife <yellowafterlife@hotmail.com>
2022-12-09 13:42:30 +0200

    Made packet types an enum abstract

--- steam/net/Packet.hx
+++ steam/net/Packet.hx
@@ -7,18 +7,18 @@ import gml.io.Buffer;
  */
 @:native("steam_net_packet")
 extern class Packet {
-	static function send(id:SteamID, data:Buffer, size:Int, type:Int):Bool;
+	static function send(id:SteamID, data:Buffer, size:Int, type:PacketType):Bool;
 	@:native("send_raw")
-	static function sendRaw(idh:Int, idl:Int, data:Buffer, size:Int, type:Int):Bool;
+	static function sendRaw(idh:Int, idl:Int, data:Buffer, size:Int, type:PacketType):Bool;
 	static function receive():Bool;
 	@:native("get_size") static function getSize():Int;
 	@:native("get_data") static function getData(buf:Buffer):Bool;
 	@:native("get_sender_id") static function getSender():SteamID;
 }
 
-extern class PacketType {
-	static inline var UNRELIABLE = 0;
-	static inline var UNRELIABLE_NODELAY = 1;
-	static inline var RELIABLE = 2;
-	static inline var RELIABLE_BUFFER = 3;
+enum abstract PacketType(Int) {
+	var UNRELIABLE = 0;
+	var UNRELIABLE_NODELAY = 1;
+	var RELIABLE = 2;
+	var RELIABLE_BUFFER = 3;
 }
